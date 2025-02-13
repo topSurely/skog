5db9b3a0d7705275b035c5ea14976e5c3c55461b
Jason Geiger <jason.phil.geiger@gmail.com>
2025-02-13 13:56:44 -0700

    Added valentine mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import { useEffect, useRef } from 'react'
 import { ThreeRender } from './ThreeRender';
+import { BrowserRouter as Router, useLocation } from 'react-router-dom';
+import { Color } from 'three';
 
 function App() {
+
+  return (
+    <Router>
+      <ThreeContainer />
+    </Router>
+  )
+}
+
+function ThreeContainer() {
+  const location = useLocation();
   const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (containerRef.current != null) {
       const renderer = new ThreeRender(containerRef.current)
+      console.log(location.pathname);
+      if (location.pathname === '/skog/valentine') {
+        renderer.renderer.setClearColor(new Color("#F4AAFF"))
+      }
 
       return () => {
         console.log("killin it");
@@ -20,8 +36,7 @@ function App() {
       <div ref={containerRef} style={{ width: "100%", height: "100vh" }}>
 
       </div>
-    </>
-  )
+    </>)
 }
 
 export default App
